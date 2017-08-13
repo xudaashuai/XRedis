0218bc2093128b94317e58e123e67f25692565ac
徐大帅 <xudashuai0819@gmail.com>
2017-08-13 19:26:49 +0800

    完成所有查看操作

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,29 @@
 /**
  * Created by xudas on 2017/8/8.
  */
-const {ipcRenderer,remote} = require('electron')
+const {ipcRenderer, remote} = require('electron')
+class Node {
+    constructor(text, type, valueType) {
+        this.text = text
+        this.type = type
+        this.valueType = valueType
+        this.nodes = []
+        this.open = false
+        this.selected = false;
+    }
+
+    clearSelect() {
+        this.selected = false;
+        for (let i = 0; i < this.nodes.length; i++) {
+            this.nodes[i].clearSelect();
+        }
+    }
+
+    select() {
+        tree.clearSelect();
+        this.selected = true;
+    }
+}
 NODE_TYPE = {
     CONNECTION: 1,
     DATABASE: 2,
@@ -19,12 +41,12 @@ VALUE_TYPE = {
 Vue.component('tree-item', {
     props: ['node'],
     methods: {
-        click: function (event) {
-            let items= document.querySelectorAll(".tree-item-text")
-            for(let i =0;i<items.length;i++){
-                items[i].select=false
+        click:function (event) {
+            if(this.node.type === NODE_TYPE.KEY){
+                this.select()
             }
-
+        },
+        spanClick: function (event) {
             switch (this.node.type) {
                 case NODE_TYPE.CONNECTION:
                 case NODE_TYPE.NAMESPACE:
@@ -32,6 +54,9 @@ Vue.component('tree-item', {
                     break
                 case NODE_TYPE.DATABASE:
                     this.node.open = !this.node.open
+                    break
+                case NODE_TYPE.KEY:
+                    this.select()
                     break
             }
         },
@@ -50,16 +75,20 @@ Vue.component('tree-item', {
                     this.node.open = true
                     break
                 case NODE_TYPE.KEY:
-                    show(this.node,table,info,page)
+                    this.select()
                     break
             }
+        },
+        select: function () {
+            tree.selectedNode = this.node
+            this.node.select();
+            show(this.node, table, info, page)
         }
     },
-    template: `
-                        <div>
-                            <p class="tree-item-text" v-on:dblclick="dbclick" :style="'background-color:hsl(0,0%,'+(100-node.type*5).toString()+'%);'+'padding-left:'+(node.type*15).toString()+'px'">
+    template: `<div>
+                            <p :class="'tree-item-text '+(node.selected?'selected ':'')+('node-'+node.type.toString())" v-on:dblclick="dbclick" v-on:click="click" >
                             <span :class="node.end?'glyphicon glyphicon-menu-right':node.open?'glyphicon glyphicon-minus':'glyphicon glyphicon-plus'"
-                                aria-hidden="true"style="margin-right: 5px" v-on:click="click"></span>
+                                aria-hidden="true"style="margin-right: 5px" v-on:click.stop="spanClick"></span>
                             {{node.text}}</p>
                                <tree-item
                                   v-if="node.open"
@@ -72,21 +101,63 @@ Vue.component('tree-item', {
 var tree = new Vue({
     el: '#root',
     data: {
-        node: {
-            text: '1',
-            nodes: [],
-            deep: 1,
-            end: false,
-            open: true,
-            type: NODE_TYPE.CONNECTION
+        node: new Node('新建连接', NODE_TYPE.CONNECTION),
+        selectedNode: null
+    },
+    methods: {
+        clearSelect: function () {
+            this.node.clearSelect()
         }
     }
 })
+
+var page = new Vue({
+    el: "#page-control",
+    data: {
+        pageCount: 100,
+        cursor: 1,
+        inputCursor: 1,
+        addable: false,
+        show: false,
+        pageSize: 100,
+        inputPageSize: 100,
+        type:VALUE_TYPE.string
+    },
+    methods: {
+        refresh: function () {
+            let cur = parseInt(this.inputCursor)
+            let pag = parseInt(this.inputPageSize)
+            if (pag >= 0) {
+                this.pageSize = pag
+            }
+            if ( cur <= this.pageCount && cur > 0) {
+                this.cursor=cur
+            }
+            show(tree.selectedNode, table, info, page)
+        },
+        clear: function () {
+            this.pageCount = null;
+            this.cursor = 1;
+        },
+    }
+})
 Vue.component('tr-item', {
-    props: ['res','index'],
+    props: ['res', 'index','type'],
     template: `
-            <tr><td>{{index}}</td><td>{{res===null?"'null'":typeof res==='string'?"'"+res+"'":res}}</td></tr>
-            `
+<tr v-if=" type !=='hash'">
+    <td>{{getIndex()}}</td>
+    <td>{{typeof res==='string'?"'"+res+"'":res}}</td>
+</tr>
+<tr v-else>
+    <td>{{getIndex()}}</td>
+    <td>{{typeof res[0]==='string'?"'"+res[0]+"'":res[0]}}</td>
+    <td>{{typeof res[1]==='string'?"'"+res[1]+"'":res[1]}}</td>
+</tr>`,
+    methods: {
+        getIndex: function () {
+            return this.index + (page.cursor - 1) * page.pageSize;
+        }
+    }
 })
 var table = new Vue({
     el: '#table',
@@ -94,14 +165,14 @@ var table = new Vue({
         result: [null]
     }
 })
-var info=new Vue({
-    el:'#info',
-    data:{
-        key:null,
-        show:false,
-        type:null,
-        length:null,
-        rename:false
+var info = new Vue({
+    el: '#info',
+    data: {
+        key: null,
+        show: false,
+        type: null,
+        length: null,
+        rename: false,
     }
 })
 ipcRenderer.on('load_client', function (event) {
@@ -110,36 +181,11 @@ ipcRenderer.on('load_client', function (event) {
     client.dbsize([], (err, mes) => {
         console.log(mes)
     })
-    tree.node.text = '新建连接'
-    tree.node.nodes = []
+    tree.node = new Node('新建连接', NODE_TYPE.CONNECTION)
     reloadDatabase(tree.node)
 })
-ipcRenderer.sendSync('connect',{
-    name:"",
-    host:"101.236.6.203",
-    port:"6379",
-})
-var page=new Vue({
-    el:"#page-control",
-    data:{
-        pageCount:100,
-        cursor:1,
-        addable:false,
-        show:false,
-        pageSize:100
-    },
-    methods:{
-        nextPage:function(){
-            if (this.cursor<this.pageSum){
-                this.cursor+=1;
-            }
-        },
-        prePage:function () {
-
-        },
-        clear:function(){
-            this.pageCount=null;
-            this.cursor=1;
-        }
-    }
+ipcRenderer.sendSync('connect', {
+    name: "",
+    host: "101.236.6.203",
+    port: "6379",
 })
--- app/js/redis.js
+++ app/js/redis.js
@@ -1,23 +1,15 @@
 /**
  * Created by xudas on 2017/8/12.
  */
-const redis = require('ioredis')
+const redis = require('redis')
 function reloadKeys(node) {
     client.select(parseInt(node.text[node.text.length - 1]), function () {
         client.keys('*', function (err, mes) {
             node.nodes = []
             for (let i = 0; i < mes.length; i++) {
-                client.type(mes[i],function (err,mess) {
+                client.type(mes[i], function (err, mess) {
                     console.log(mess)
-                    node.nodes.push({
-                        id: 1,
-                        nodes: [],
-                        text: mes[i],
-                        valueType:VALUE_TYPE[mess],
-                        end: true,
-                        open: false,
-                        type: NODE_TYPE.KEY
-                    })
+                    node.nodes.push(new Node(mes[i],NODE_TYPE.KEY,VALUE_TYPE[mess]))
                 })
             }
         })
@@ -26,61 +18,74 @@ function reloadKeys(node) {
 function reloadDatabase(node) {
     client.config('GET', "databases", function (err, mes) {
         dbsize = mes[1]
-        node.nodes=[]
+        node.nodes = []
         for (let i = 0; i < dbsize; i++) {
-            node.nodes.push({
-                id: i,
-                nodes: [],
-                text: "DB" + i.toString(),
-                deep: 2,
-                type: NODE_TYPE.DATABASE,
-                end: false,
-                open: false
-            })
+            node.nodes.push(new Node("DB" + i.toString(), NODE_TYPE.DATABASE, null))
         }
+        node.open=true
     })
 }
 
-function show(node,table,info,page) {
-    table.type=node.valueType
-    info.type=node.valueType
-    info.key=node.text
-    info.show=true
-    info.length=null
-    let cursor=page.cursor
-    let pageSize=page.pageSize
-    console.log(node.valueType)
-    switch (node.valueType){
+function show(node, table, info, page) {
+    table.type = node.valueType
+    info.type = node.valueType
+    info.key = node.text
+    info.show = true
+    info.length = null
+    let cursor = page.cursor
+    let pageSize = page.pageSize
+    console.log(cursor)
+    console.log(pageSize)
+    switch (node.valueType) {
         case VALUE_TYPE.string:
-            client.get(node.text,function (err,mes) {
+            info.length=1
+            client.get(node.text, function (err, mes) {
                 console.log(mes)
-                table.result=[mes,]
+                table.result = [mes,]
             })
             break;
         case VALUE_TYPE.set:
-            client.sscan(node.text, (cursor-1)*pageSize, 'COUNT', cursor*pageSize,function (err,mes) {
-                table.result=mes[1]
-                info.length=mes[0]
-                page.pageCount=Math.ceil(info.length/pageSize)
+            client.scard(node.text,function (err,mes) {
+                info.length = mes
+                page.pageCount = Math.ceil(info.length / pageSize)
+            })
+            client.sscan(node.text, (cursor - 1) * pageSize, 'COUNT', pageSize, function (err, mes) {
+                table.result = mes[1]
             })
             break;
         case VALUE_TYPE.zset:
-            client.zscan(node.text,(cursor-1)*pageSize, 'COUNT', cursor*pageSize,function (err,mes) {
-                table.result=mes[1]
-                info.length=mes[0]
-                page.pageCount=Math.ceil(info.length/pageSize)
+            client.zcard(node.text,function (err,mes) {
+                info.length = mes
+                page.pageCount = Math.ceil(info.length / pageSize)
+            })
+            client.zscan(node.text, (cursor - 1) * pageSize, 'COUNT', pageSize, function (err, mes) {
+                table.result = mes[1]
+                console.log(mes)
             })
             break;
         case VALUE_TYPE.hash:
-            client.get(node.text,function (err,mes) {
-                table.result=mes
+            client.hlen(node.text,function (err,mes) {
+                info.length = mes
+                page.pageCount = Math.ceil(info.length / pageSize)
+            })
+            client.hscan(node.text, (cursor - 1) * pageSize, 'COUNT', pageSize, function (err, mes) {
+                console.log(mes)
+                mes=mes[1]
+                let m=[]
+                for(let i=0;i<mes.length;i+=2){
+                    m.push([mes[i],mes[i+1]])
+                }
+                table.result = m
             })
             break;
         case VALUE_TYPE.list:
-            client.get(node.text,function (err,mes) {
-                table.result=mes[1]
-                info.length=mes[0]
-                page.pageCount=Math.ceil(info.length/pageSize)
+            client.llen(node.text,function (err,mes) {
+                info.length = mes
+                page.pageCount = Math.ceil(info.length / pageSize)
+            })
+            client.lrange(node.text, (cursor - 1) * pageSize,cursor*pageSize-1,function (err, mes) {
+                table.result = mes
+                console.log(mes)
             })
             break;
     }

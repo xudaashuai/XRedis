8bc192a79c230993d30f3526892e79178be44f36
徐大帅 <xudashuai0819@gmail.com>
2017-08-16 00:10:28 +0800

    完善添加连接功能

--- app/js/connection.js
+++ app/js/connection.js
@@ -7,17 +7,31 @@ let button = document.getElementById('submit')
 function getValue(id) {
     return document.getElementById(id).value
 }
-button.onclick = function (event) {
-    if (ipcRenderer.sendSync('add_connect', {
-            name: getValue('name'),
-            host: getValue('host'),
-            port: getValue('port'),
-            pass: getValue('pass')
-        })) {
-        console.log('yes')
-        remote.getCurrentWindow().close()
-    } else {
-        console.log('no')
-        alert('连接名重复了')
+let app=new Vue({
+    el:'#form',
+    data:{
+        data:{
+            port:'6379',
+            host:'127.0.0.1',
+            name:'新建连接',
+            pass:''
+        }
+    },
+    methods: {
+        submit: function (events) {
+            Object.keys(this.data).forEach(key => {
+                if (key !== 'pass' && this.data[key].length === 0) {
+                    alert(key + '不能为空')
+                    return
+                }
+            })
+            if (ipcRenderer.sendSync('add_connect', this.data)) {
+                console.log('yes')
+                remote.getCurrentWindow().close()
+            } else {
+                console.log('no')
+                alert('连接名重复了')
+            }
+        }
     }
-}
+})
